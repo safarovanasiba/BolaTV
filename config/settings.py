629087608e113363cecbd50eb2469e6f3b26e7b3
Bahtiyorjon Tolipov <tolipovbaxtiyor99@gmail.com>
2025-05-01 01:28:21 +0900

    Update database configuration for Railway

--- config/settings.py
+++ config/settings.py
@@ -85,9 +85,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 #     }
 # }
 
+# Database configuration
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(BASE_DIR, "db.sqlite3")),
         conn_max_age=600,
         ssl_require=True
     )
